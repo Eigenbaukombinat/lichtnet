45a41925d43f2c9f5987b68ad855fe51950cb936
nilo <dh@gocept.com>
2016-11-22 23:22:46 +0100

    first commit

---
+++ dmxmodel.py
@@ -0,0 +1,138 @@
+import artnet
+
+
+class Channel(object):
+    """Describes a channel"""
+
+    def __init__(self, channel_id, description='Generic Channel'):
+        self._description = description
+        self.set_channel_id(channel_id)
+        self.value = 0x00
+        self.old_value = 0x00
+
+    def set_channel_id(self, channel_id):
+        self.channel_id = channel_id
+        self.description = '{} (#{})'.format(self._description, channel_id)
+
+    def blackout(self):
+        self.value = 0x00
+
+
+class Fixture(object):
+    """Base class for an artnet fixture."""
+
+    def __init__(self):
+        self.num_channels = 0
+        self.address = '255.255.255.255'
+        self.channels = {}
+        
+    def add_channel(self, name, descr):
+        self.channels[name] = Channel(name, descr)
+        self.num_channels = len(self.channels.keys())
+
+    def set_channel(self, name, value):
+        channel = self.channels[name]
+        channel.value = value
+
+    def blackout(self):
+        for channel in self.channels.values():
+            channel.blackout()
+
+    def set_base_channel(self, base_channel):
+        for name, channel in  self.channels.items():
+            channel.set_channel_id(channel.channel_id + base_channel)
+
+
+class RGBFixture(Fixture):
+
+    def __init__(self):
+        self.num_channels = 4
+        self.address = '255.255.255.255'
+        self.channels = {'r': Channel(0, 'Red'),
+                    'g': Channel(1, 'Green'),
+                    'b': Channel(2, 'Blue'),
+                    'm': Channel(3, 'Main'),}
+
+    def set_color(self, r, g, b):
+        self.set_channel('r', r)
+        self.set_channel('g', g)
+        self.set_channel('b', b)
+    
+    def set_main_brightness(self, brightness):
+        self.set_channel('m', brightness)
+
+
+
+
+class Universe(object):
+    address = '255.255.255.255'
+    data = 512*[0]
+    fixture_channels = 512*[None]
+
+    def __init__(self, artnet):
+        self.address = '255.255.255.255'
+        self.data = 512*[0]
+        self.fixture_channels = 512*[None]
+        self.artnet = artnet
+
+    def register_fixture_channels(self, fix):
+        fix.universe = self
+        for name, channel in fix.channels.items():
+            self.fixture_channels[channel.channel_id] = channel
+
+    def update_data(self):
+        for ch in self.fixture_channels:
+            if ch is None:
+                continue
+            self.data[ch.channel_id] = ch.value
+        self.send()
+
+    def blackout(self):
+        for ch in self.fixture_channels:
+            if ch is not None:
+                ch.blackout()
+        self.send()
+        
+    def send(self):
+        data = self.artnet.encode_channels(*zip(range(len(self.data)), self.data))
+        self.artnet.sendArtDMX(self.address, data)
+
+FIXTURES = {'rgb': RGBFixture}
+
+class Controller(object):
+    universes = {}
+    fixtures = {}
+
+    def __init__(self):
+        self.universes = {}
+        self.fixtures = {}
+        self.artnet = artnet.ArtNet()
+
+    def add_fixture(self, name, type_, base_channel, address):
+        fix = FIXTURES[type_]()
+        fix.set_base_channel(base_channel)
+        fix.address = address
+        if not self.universes.has_key(address):
+            universe = Universe(self.artnet)
+            universe.address = address
+            self.universes[address] = universe
+       
+        self.universes[address].register_fixture_channels(fix)
+        self.fixtures[name] = fix
+
+    def get_fixture(self, name):
+        return self.fixtures[name]
+
+    def send_update(self, fixture_names=None):
+        if fixture_names is None:
+            universes = set(self.universes.values())
+        else:
+            universes = set()
+            for name in fixture_names:
+                fix = self.fixtures[name]
+                universes.add(self.universes[fix.address])
+            
+        for universe in universes:
+            universe.update_data()
+
+
---
+++ server.py
@@ -0,0 +1,50 @@
+import logging
+from flask import Flask, render_template
+import dmxmodel
+
+
+log = logging.getLogger('DMXWeb')
+
+
+controller = dmxmodel.Controller()
+
+controller.add_fixture('RGB1', 'rgb', 0, '10.110.115.10')
+controller.add_fixture('RGB2', 'rgb', 4, '10.110.115.10')
+controller.add_fixture('RGB3', 'rgb', 8, '10.110.115.10')
+
+fixtures = {'r1': controller.get_fixture('RGB1'),
+    'r2': controller.get_fixture('RGB2'),
+    'r3': controller.get_fixture('RGB3')}
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('index.html')
+
+@app.route('/jquery.js')
+def jquery():
+    return render_template('jquery-3.1.1.min.js')
+
+@app.route('/color.js')
+def colorjs():
+    return render_template('jqColorPicker.min.js')
+
+@app.route('/index.css')
+def css():
+    return render_template('index.css')
+
+
+def hex_to_int(h):
+    return (int(h[0:2], 16),
+        int(h[2:4], 16),
+        int(h[4:6], 16))
+
+@app.route('/color/<fixture>/<color>/<brightness>')
+def color(fixture, color, brightness):
+    fix = fixtures[fixture]
+    fix.set_color(*hex_to_int(color))
+    fix.set_main_brightness(int(brightness))
+    controller.send_update()
+    return 'OK'
+
